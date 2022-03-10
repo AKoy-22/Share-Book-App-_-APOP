66ac77a3c679bd5ece0fd88f862ff5b648225180
AKoy-22 <100966481+AKoy-22@users.noreply.github.com>
2022-03-09 23:36:42 -0800

    Added Database

---
+++ app/src/main/java/com/example/apopsharebook/Database.java
@@ -0,0 +1,120 @@
+package com.example.apopsharebook;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper  {
+
+    final static String DATABASE_NAME="APOP.db";
+    final static int DATABASE_VERSION=2;
+    //USER TABLE
+    final static String U_TABLE="User_table";
+    final static String U_UserId="UserId"; //PK - email
+    final static String U_Pw="Password";
+    final static String U_FName="FName";
+    final static String U_LName="LName";
+    final static String U_Address="Address";
+    final static String U_UserType="UserType"; //User or Admin
+
+    //BOOK TABLE
+    final static String B_TABLE="Book_table";
+    final static String B_BookId="BookId"; //PK autoincrement
+    final static String B_ISBN="ISBN";  //integer
+    final static String B_Title="Title";
+    final static String B_Genre="Genre";
+    final static String B_Author="Author";
+    final static String B_OwnerId="OwnerId"; //one of UserIds FK
+    final static String B_Status="Status"; //on Loan or Available
+
+    //LOAN TABLE
+    final static String L_TABLE="Loan_table";
+    final static String L_LoanId="LoanId"; //PK auto increment
+    final static String L_BookId="BookId"; //FK integer
+    final static String L_BorrowerId="BorrowerId"; //one of UserIds FK
+    final static String L_StartDate="StartDate";
+    final static String L_ReturnDate="ReturnDate";
+    final static String L_Price="Price";
+
+    //PREFERENCE TABLE
+    final static String P_TABLE="Preference_table";
+    final static String P_UserId="UserId";
+    final static String P_Preference="Preference";
+
+    //READING HISTORY TABLE
+    final static String R_TABLE="RHistory_table";
+    final static String R_UserId="UserId";
+    final static String R_BookId="BookId";
+    final static String R_Note="Note";
+
+    //MESSAGE TABLE
+    final static String M_TABLE="Message_table";
+    final static String M_MessageId="MessageId"; //PK integer autoincrement
+    final static String M_SenderId="SenderId"; //One of userId FK
+    final static String M_ReceiverId="ReceiverId"; //One of userId FK
+    final static String M_BookId="BookId"; //FK integer
+    final static String M_MsgDate="MsgDate";
+
+
+
+    public Database(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        SQLiteDatabase db=this.getWritableDatabase();
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db){
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON");
+        }
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //create User Table
+       String createTableQuery=" CREATE TABLE "+U_TABLE+"("+ U_UserId+ " text PRIMARY KEY,"+ U_Pw+" text,"+U_FName+" text,"
+                +U_LName+" text,"+U_Address+" text,"+U_UserType+" text)";
+        sqLiteDatabase.execSQL(createTableQuery);
+        //create Book Table
+        createTableQuery=" CREATE TABLE "+B_TABLE+ "("+ B_BookId+ " integer,"+ B_ISBN+" integer,"+B_Title+" text,"
+                +B_Genre+" text,"+B_Author+" text,"+B_OwnerId+" text,"+B_Status+" text,"+
+                "PRIMARY KEY ("+ B_BookId+ "), FOREIGN KEY ("+B_OwnerId+") REFERENCES "+U_TABLE+" ("+U_UserId+"))";
+        sqLiteDatabase.execSQL(createTableQuery);
+        //create Loan Table
+        createTableQuery=" CREATE TABLE "+L_TABLE+ "("+ L_LoanId+ " integer,"+ L_BookId+" integer,"+L_BorrowerId+" text,"
+                +L_StartDate+" text,"+L_ReturnDate+" text,"+L_Price+" text,"+
+                "PRIMARY KEY ("+ L_LoanId+ "), FOREIGN KEY ("+L_BookId+") REFERENCES "+B_TABLE+" ("+B_BookId+"),  " +
+                "FOREIGN KEY ("+L_BorrowerId+") REFERENCES "+U_TABLE+" ("+U_UserId+"))";
+        sqLiteDatabase.execSQL(createTableQuery);
+        //create Preference Table
+        createTableQuery=" CREATE TABLE "+P_TABLE+ "("+ P_UserId+ " text,"+ P_Preference+" text,"+
+                "PRIMARY KEY ("+ P_UserId+ ","+P_Preference+"), FOREIGN KEY ("+P_UserId+") REFERENCES "+U_TABLE+" ("+U_UserId+"))";
+        sqLiteDatabase.execSQL(createTableQuery);
+        //create Reading History Table
+        createTableQuery=" CREATE TABLE "+R_TABLE+ "("+ R_UserId+ " text,"+ R_BookId+" integer,"+ R_Note+" text,"+
+                "PRIMARY KEY ("+ R_UserId+ ","+R_BookId+"), FOREIGN KEY ("+R_UserId+") REFERENCES "+U_TABLE+" ("+U_UserId+"))";
+        sqLiteDatabase.execSQL(createTableQuery);
+        //create Message Table
+        createTableQuery=" CREATE TABLE "+M_TABLE+ "("+ M_MessageId+ " integer,"+ M_SenderId+" integer,"+ M_ReceiverId+" text,"+M_BookId+" integer,"+ M_MsgDate+" text,"+
+                "PRIMARY KEY ("+M_MessageId+"), FOREIGN KEY ("+M_SenderId+") REFERENCES "+U_TABLE+" ("+U_UserId+"), " +
+                "FOREIGN KEY ("+M_ReceiverId+") REFERENCES "+U_TABLE+" ("+U_UserId+"))";
+        sqLiteDatabase.execSQL(createTableQuery);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+U_TABLE);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+B_TABLE);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+L_TABLE);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+P_TABLE);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+R_TABLE);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+M_TABLE);
+        onCreate(sqLiteDatabase);
+
+    }
+}
--- app/src/main/java/com/example/apopsharebook/Login.java
+++ app/src/main/java/com/example/apopsharebook/Login.java
@@ -8,12 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+Database db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        //creating Database object
+        db=new Database(this);
         Button btnLoginMain = findViewById(R.id.btnLogin1);
 
         btnLoginMain.setOnClickListener(v -> startActivity(new Intent(Login.this,MainMenu.class)));
