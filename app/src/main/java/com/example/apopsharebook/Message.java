b929e0e60cddb41a4f81715721fc3d4bcf743d4e
oprah86 <oprah8674@gmail.com>
2022-03-23 00:22:59 -0700

    Removed NestedScrollView, using ListView in Message Page

--- app/src/main/java/com/example/apopsharebook/Message.java
+++ app/src/main/java/com/example/apopsharebook/Message.java
@@ -5,9 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Message extends AppCompatActivity {
+
+    //fake data
+    String[] messageTitle = {"A request from abc0123","A message from cvb3der","A message from 412dd2"};
+    String[] messageDate = {"2022-03-02","2022-03-08","2022-03-11","2022-03-21"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +27,6 @@ public class Message extends AppCompatActivity {
 
         ImageButton go_back = findViewById(R.id.btnBack);
         BottomNavigationView bottom_menu = findViewById(R.id.bottom_menu);
-        ImageButton btn_message = findViewById(R.id.btnMessageIcon);
 
         //button event for go back main page
         go_back.setOnClickListener(v -> startActivity(new Intent(Message.this,MainMenu.class)));
@@ -36,5 +46,25 @@ public class Message extends AppCompatActivity {
             }
             return true;
         });
+
+        //ListView
+        List<HashMap<String,String>> messageList = new ArrayList<>();
+
+        for(int i=0; i<messageTitle.length; i++) {
+            HashMap<String,String> hashmap = new HashMap<>();
+            hashmap.put("title",messageTitle[i]);
+            hashmap.put("date",messageDate[i]);
+            messageList.add(hashmap);
+        }
+
+        String[] from = {"title","date"};
+        int[] to = {R.id.txtMessage,R.id.txtMessageDate};
+
+        SimpleAdapter adapter = new SimpleAdapter(getBaseContext(),messageList,
+                R.layout.message_list_item,from,to);
+
+        ListView listView = findViewById(R.id.CureentLoanListView);
+        listView.setAdapter(adapter);
+
     }
 }
