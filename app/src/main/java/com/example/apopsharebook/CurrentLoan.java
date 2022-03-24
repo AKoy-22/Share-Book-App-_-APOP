fa037c7b7b9e39a45d77a02a173bc4396b857b84
oprah86 <oprah8674@gmail.com>
2022-03-23 23:41:35 -0700

    Update ListView using ArrayList in CurrentLoan, MainMenu

--- app/src/main/java/com/example/apopsharebook/CurrentLoan.java
+++ app/src/main/java/com/example/apopsharebook/CurrentLoan.java
@@ -5,9 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class CurrentLoan extends AppCompatActivity {
+
+    List<CurrentLoanList> clList;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +47,25 @@ public class CurrentLoan extends AppCompatActivity {
             }
             return true;
         });
+
+        //Using ListView and ArrayList
+        String[] from = {"cover","title","author","publisher","year","category","owner","date"};
+        int[] to = {R.id.imgBookCover,R.id.txtBookTitle,R.id.txtBookAuthor,R.id.txtBookPublisher,
+                    R.id.txtBookYear,R.id.txtBookCategory,R.id.txtBookOwner,R.id.txtBorrowBookDate};
+
+        clList = new ArrayList<>();
+        clList.add(new CurrentLoanList(R.drawable.cover01,"Cloud Cuckoo Land","Anthony Doerr",
+                "Scribner","2021","Fiction","mary11","2022-03-06"));
+        clList.add(new CurrentLoanList(R.drawable.cover02,"One Two Three","Laurie Frankel",
+                "Henry Holt and Co","2021","NonFiction","tom22","2022-03-08"));
+        clList.add(new CurrentLoanList(R.drawable.cover03,"A Court of Silver Flames","Sarah J. Maas",
+                "Bloomsbury Publishing","2021","Fiction","allen33","2022-03-19"));
+        clList.add(new CurrentLoanList(R.drawable.cover04,"Under the Whispering Door","TJ Klune",
+                "Tor Books","2021","Fiction","joe55","2022-03-21"));
+
+        listView = findViewById(R.id.CureentLoanListView);
+        CurrentLoanListAdapter adapter = new CurrentLoanListAdapter(this,R.layout.currentloan_list_item,clList);
+        listView.setAdapter(adapter);
+        listView.setDivider(null);
     }
 }
